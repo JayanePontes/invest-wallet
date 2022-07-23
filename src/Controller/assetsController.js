40d85f0af8688353f3ba10a9141e55d01a371e21
Jayane Pontes <pontesjayane@gmail.com>
2022-07-23 14:11:40 -0300

    Corrgindo erros de lint

--- src/Controller/assetsController.js
+++ src/Controller/assetsController.js
@@ -6,7 +6,7 @@ const assetsController = {
 
     const asset = await assetsService.getAsset(codAsset);
 
-    if (asset === 'error') return res.status(400).json({ message: 'codAsset inexistente'});
+    if (asset === 'error') return res.status(400).json({ message: 'codAsset inexistente' });
 
     res.status(200).json(asset);
   },
